a5d2a4f1a3148b5103e76a8906a7169c7cf1b3f0
Kevin <kevcoop1999@gmail.com>
2026-04-01 20:53:02 -0400

    Now this should work of .env file

--- easy_apply.py
+++ easy_apply.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+load_dotenv()
 USERNAME = os.environ.get("LINKEDIN_USERNAME")
 PASSWORD = os.environ.get("LINKEDIN_PASSWORD")
 
